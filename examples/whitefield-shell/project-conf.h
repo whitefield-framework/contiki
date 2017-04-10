4f247da27b81f468016f7441602f1321d0620eeb
nyrahul <nyrahul@gmail.com>
2017-04-10 08:01:26 +0530

    added whitefield mac driver for contiki

--- examples/whitefield-shell/project-conf.h
+++ examples/whitefield-shell/project-conf.h
@@ -1,9 +1,8 @@
 #ifndef _PROJECT_CONF_H_
 #define _PROJECT_CONF_H_
 
-#ifndef NETSTACK_CONF_MAC
-#define NETSTACK_CONF_MAC     nullmac_driver
-#endif /* NETSTACK_CONF_MAC */
+#undef NETSTACK_CONF_MAC
+#define NETSTACK_CONF_MAC     wfmac_driver
 
 #ifndef NETSTACK_CONF_RDC
 #define NETSTACK_CONF_RDC     nullrdc_driver
