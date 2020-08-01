5aa7f631f4be564b1874133c8046265b8a033770
weld10 <50168266+Amrican10@users.noreply.github.com>
2020-08-01 17:12:45 +0200

    Update project-conf.h

--- examples/containers/project-conf.h
+++ examples/containers/project-conf.h
@@ -2,6 +2,75 @@
 #ifndef __PROJECT_CONF_H__
 #define __PROJECT_CONF_H__
 
+/********************************************************************/
+#ifndef PROJECT_CONF_H_
+#define PROJECT_CONF_H_
+
+#ifndef WITH_NON_STORING
+#define WITH_NON_STORING 0 /* Set this to run with non-storing mode */
+#endif /* WITH_NON_STORING */
+
+#undef NBR_TABLE_CONF_MAX_NEIGHBORS
+#undef UIP_CONF_MAX_ROUTES
+
+#ifdef TEST_MORE_ROUTES
+/* configure number of neighbors and routes */
+#define NBR_TABLE_CONF_MAX_NEIGHBORS     10
+#define UIP_CONF_MAX_ROUTES   30
+#else
+/* configure number of neighbors and routes */
+#define NBR_TABLE_CONF_MAX_NEIGHBORS     100
+#define UIP_CONF_MAX_ROUTES   100
+#endif /* TEST_MORE_ROUTES */
+
+#undef NETSTACK_CONF_RDC
+#define NETSTACK_CONF_RDC     nullrdc_driver
+#undef NULLRDC_CONF_802154_AUTOACK
+#define NULLRDC_CONF_802154_AUTOACK       1
+
+/* Define as minutes */
+#define RPL_CONF_DEFAULT_LIFETIME_UNIT   60
+
+/* 10 minutes lifetime of routes */
+#define RPL_CONF_DEFAULT_LIFETIME        10
+
+#define RPL_CONF_DEFAULT_ROUTE_INFINITE_LIFETIME 1
+#define SCSLOWPAN_CONF_FRAG_FORWARDING 0
+#define SICSLOWPAN_CONF_REASS_CONTEXTS 10
+
+#define SERVER_REPLY 1
+
+#if WITH_NON_STORING
+#undef RPL_NS_CONF_LINK_NUM
+#define RPL_NS_CONF_LINK_NUM 40 /* Number of links maintained at the root. Can be set to 0 at non-root nodes. */
+#undef UIP_CONF_MAX_ROUTES
+#define UIP_CONF_MAX_ROUTES 0 /* No need for routes */
+#undef RPL_CONF_MOP
+#define RPL_CONF_MOP RPL_MOP_NON_STORING /* Mode of operation*/
+#endif /* WITH_NON_STORING */
+
+#define RPL_CONF_WITH_DCO 1
+
+#endif
+
+
+/********************************************************************/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 #define USE_METRIC_CONTAINERS 
 
 #ifdef USE_METRIC_CONTAINERS
